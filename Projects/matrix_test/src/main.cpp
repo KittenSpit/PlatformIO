c2a96e3c09364e9720fc808451acb7e259ebdabc
David Adam <david.adam@andura.ca>
2025-08-16 23:23:24 -0400

    add text from serial mon

--- Projects/matrix_test/src/main.cpp
+++ Projects/matrix_test/src/main.cpp
@@ -9,107 +9,32 @@ ArduinoLEDMatrix matrix;
 void setup() {
   Serial.begin(9600);
   matrix.begin();
+  matrix.textFont(Font_5x7);
+  matrix.stroke(0xFFFFFFFF);
+  matrix.textScrollSpeed(100);
 }
 
-const uint32_t happy[] = {
-    0x19819,
-    0x80000001,
-    0x81f8000
-};
-const uint32_t heart[] = {
-    0x3184a444,
-    0x44042081,
-    0x100a0040
-};
-  
-
-uint8_t frame[8][12] = {
-  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
-};
-
-void leftEye(){
-  //Left eye
-  frame[1][3] = 1;
-  frame[1][4] = 1;
-  frame[2][3] = 1;
-  frame[2][4] = 1;
-}
-
-void wink(){
-  //Wink with the left eye
-  frame[1][3] = 0;
-  frame[1][4] = 0;
-  frame[2][3] = 1;
-  frame[2][4] = 1;
-}
-
-void rightEye(){
-  //Right eye
-  frame[1][8] = 1;
-  frame[1][9] = 1;
-  frame[2][8] = 1;
-  frame[2][9] = 1;
-}
-
-void mouth(){
-  //Mouth
-  frame[5][3] = 1;
-  frame[5][9] = 1;
-  frame[6][3] = 1;
-  frame[6][4] = 1;
-  frame[6][5] = 1;
-  frame[6][6] = 1;
-  frame[6][7] = 1;
-  frame[6][8] = 1;
-  frame[6][9] = 1;
-}
+String a;
 
 void loop(){
-leftEye();
-rightEye();
-mouth();
-
-matrix.renderBitmap(frame, 8, 12);
-
-delay(1000);
-wink();
-
-matrix.renderBitmap(frame, 8, 12);
-delay(1000);
-
-matrix.loadFrame(happy);
-  delay(1000);
-
-  matrix.loadFrame(heart);
-  delay(1000);
-
-  delay(5000);
-// Make it scroll!
+//  matrix.beginDraw();
+while(Serial.available()) {
+a= Serial.readString();// read the incoming data as string
+Serial.println(a);
+}
+ 
   matrix.beginDraw();
-
   matrix.stroke(0xFFFFFFFF);
-  matrix.textScrollSpeed(50);
+  matrix.textScrollSpeed(100);
 
   // add the text
-  int a;
-  a = 5;
-  
-  char text[] = "    Hello World!    ";
-  
+// const char text[] = " Hello World! ";
   matrix.textFont(Font_5x7);
   matrix.beginText(0, 1, 0xFFFFFF);
-  matrix.println(text);
+  matrix.println(a);
   matrix.endText(SCROLL_LEFT);
 
   matrix.endDraw();
 
-  delay(5000);
-
+  
 }
