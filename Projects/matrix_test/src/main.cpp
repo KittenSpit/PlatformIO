37e81e66b24387898f6a7ff6bd4058013c044626
David Adam <david.adam@andura.ca>
2025-08-16 20:11:40 -0400

    add scroll text

--- Projects/matrix_test/src/main.cpp
+++ Projects/matrix_test/src/main.cpp
@@ -1,13 +1,27 @@
 #include <Arduino.h>
-
+#include "ArduinoGraphics.h"
 #include "Arduino_LED_Matrix.h"
 // david edit
 ArduinoLEDMatrix matrix;
 
+
+
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
   matrix.begin();
 }
+
+const uint32_t happy[] = {
+    0x19819,
+    0x80000001,
+    0x81f8000
+};
+const uint32_t heart[] = {
+    0x3184a444,
+    0x44042081,
+    0x100a0040
+};
+  
 
 uint8_t frame[8][12] = {
   { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
@@ -69,4 +83,29 @@ wink();
 
 matrix.renderBitmap(frame, 8, 12);
 delay(1000);
+
+matrix.loadFrame(happy);
+  delay(1000);
+
+  matrix.loadFrame(heart);
+  delay(1000);
+
+  delay(5000);
+// Make it scroll!
+  matrix.beginDraw();
+
+  matrix.stroke(0xFFFFFFFF);
+  matrix.textScrollSpeed(50);
+
+  // add the text
+  const char text[] = "    Hello World!    ";
+  matrix.textFont(Font_5x7);
+  matrix.beginText(0, 1, 0xFFFFFF);
+  matrix.println(text);
+  matrix.endText(SCROLL_LEFT);
+
+  matrix.endDraw();
+
+  delay(5000);
+
 }
